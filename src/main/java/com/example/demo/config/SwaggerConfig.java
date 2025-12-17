78fc44b5d0df89ea11f7f04d8bfc67a17bc6845c
bala-amypo <bala@amypo.in>
2025-12-17 06:02:24 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,15 +1,12 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(title = "Asset Management API", version = "1.0")
-)
+@OpenAPIDefinition
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
