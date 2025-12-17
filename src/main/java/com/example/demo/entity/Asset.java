93121367b2fc9b6d466e6ce8492208325b1c9e77
bala-amypo <bala@amypo.in>
2025-12-17 04:27:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Asset.java
@@ -0,0 +1,33 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.time.*;
+
+@entity
+public class Asset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String assetTag;
+
+    private String assetType;
+    private String model;
+    private LocalDate purchaseDate;
+    private String status;
+
+    @ManyToOne
+    private User currentHolder;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        if (status == null) status = "AVAILABLE";
+        if (createdAt == null) createdAt = LocalDateTime.now();
+    }
+
+    // getters & setters
+}
