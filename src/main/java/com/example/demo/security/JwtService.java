35a007968fa4da2451b260159aea386a7a174e33
bala-amypo <bala@amypo.in>
2025-12-18 06:30:31 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtService.java
+++ src/main/java/com/example/demo/security/JwtService.java
@@ -2,21 +2,30 @@ package com.example.demo.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private final String SECRET = "demo-secret-key";
+    private static final String SECRET_KEY = "digital-asset-lifecycle-secret-key-123456";
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(
+                SECRET_KEY.getBytes(StandardCharsets.UTF_8)
+        );
+    }
 
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 }
