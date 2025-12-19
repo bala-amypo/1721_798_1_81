4a90d41e1b34d4aaac61108cf14ea16307e841a7
bala-amypo <bala@amypo.in>
2025-12-19 04:41:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/Asset.java
+++ src/main/java/com/example/demo/entity/Asset.java
@@ -1,34 +1,31 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "assets")
 public class Asset {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
-    private String assetTag;
-
-    private String assetType;
-    private String model;
-    private LocalDate purchaseDate;
+    private String name;
     private String status;
 
-    @ManyToOne
-    private User currentHolder;
-
-    private LocalDateTime createdAt;
-
-    @PrePersist
-    public void init() {
-        if (status == null) status = "AVAILABLE";
-        if (createdAt == null) createdAt = LocalDateTime.now();
+    public Long getId() {
+        return id;
     }
 
-    // getters & setters
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
