e95cf161785d8c78567091a94b8df9114124aa66
bala-amypo <bala@amypo.in>
2025-12-24 14:56:43 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AssetServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AssetServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.demo.entity.Asset;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.AssetRepository;
 import com.example.demo.service.AssetService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class AssetServiceImpl implements AssetService {
 
     private final AssetRepository assetRepository;
@@ -21,19 +23,19 @@ public class AssetServiceImpl implements AssetService {
     }
 
     @Override
-    public Asset getAsset(Long id) {
-        return assetRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Asset not found"));
-    }
-
-    @Override
     public List<Asset> getAllAssets() {
         return assetRepository.findAll();
     }
 
     @Override
-    public Asset updateStatus(Long assetId, String status) {
-        Asset asset = getAsset(assetId);
+    public Asset getAsset(Long id) {
+        return assetRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Asset not found with id " + id));
+    }
+
+    @Override
+    public Asset updateStatus(Long id, String status) {
+        Asset asset = getAsset(id);
         asset.setStatus(status);
         return assetRepository.save(asset);
     }
