7407feb286684a4cc219ec916bf26088c72f0c8f
bala-amypo <bala@amypo.in>
2025-12-25 15:28:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,55 +1,25 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String fullName;
-
-    @Column(unique = true)
+    private String name;
     private String email;
-
-    private String department;
-
-    private String role;
-
     private String password;
 
-    private LocalDateTime createdAt;
-
-    public User() {}
-
-    public User(Long id, String fullName, String email, String department,
-                String role, String password, LocalDateTime createdAt) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.department = department;
-        this.role = role;
-        this.password = password;
-        this.createdAt = createdAt;
-    }
-
-    @PrePersist
-    public void prePersist() {
-        if (role == null) role = "USER";
-        if (createdAt == null) createdAt = LocalDateTime.now();
-    }
-
     public Long getId() { return id; }
-    public String getFullName() { return fullName; }
+    public String getName() { return name; }
     public String getEmail() { return email; }
-    public String getDepartment() { return department; }
-    public String getRole() { return role; }
     public String getPassword() { return password; }
 
     public void setId(Long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
     public void setPassword(String password) { this.password = password; }
 }
